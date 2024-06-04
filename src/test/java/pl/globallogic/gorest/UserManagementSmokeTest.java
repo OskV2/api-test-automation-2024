f06e44f4fadb142ce9a0f858be462c930bbd52de
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2024-06-04 21:54:56 +0200

    feat: tests code refactor, added schema validation to test

--- src/test/java/pl/globallogic/gorest/UserManagementSmokeTest.java
+++ src/test/java/pl/globallogic/gorest/UserManagementSmokeTest.java
@@ -1,54 +1,60 @@
 package pl.globallogic.gorest;
 
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pl.globallogic.configuration.ConfigurationVerificationTest;
 import pl.globallogic.gorest.dto.UserRequestDto;
 import pl.globallogic.gorest.dto.UserResponseDto;
 
-import java.util.Map;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class UserManagementSmokeTest {
 
-    private static final String HOST = "https://gorest.co.in/public/v2";
+    private static final String HOST = System.getProperty("host") + "public/v2";
     private static final String ENDPOINT = "/users";
     private static final String ENDPOINT_WITH_ID = "/users/{userId}";
-    private static final String TOKEN = "edb4afe94ffa5dd5ee817c3eb2ff3a0408777f6781098e96969cd9cb742050d4";
+    private static final String TOKEN = System.getProperty("token");
     private static Logger logger = LoggerFactory.getLogger(ConfigurationVerificationTest.class);
     private RequestSpecification req;
 
     @BeforeMethod
-    public void testSetup() {
+    public void testSetUp() {
         req = given()
-                .basePath(ENDPOINT_WITH_ID)
+                .basePath(ENDPOINT)
                 .baseUri(HOST)
                 .contentType(ContentType.JSON)
                 .header("Authorization", "Bearer " + TOKEN);
     }
 
+    @AfterMethod
+    public void testCleanUp () {
+        logger.info("Put your test clean up");
+    }
+
     @Test
     public void shouldListAllUsersInformation() {
-        given()  //  Arrange
-                .baseUri(HOST)
-                .basePath(ENDPOINT)
-                .queryParam("page", 3)
-                .queryParam("per_page", 10)
-                .log()
-                .parameters()
-        .when()  //  Act
-                .get()
-        .then()  //  Assert
-                .log().headers()
-                .statusCode(200);
+        int user_per_page = 10;
+        int target_page = 3;
+
+        req.queryParam("page", user_per_page)
+            .queryParam("per_page", target_page)
+        .when()
+            .get()
+        .then()
+            .statusCode(200)
+            .header("x-pagination-limit", equalTo(String.valueOf(user_per_page)))
+            .body("[0].id", notNullValue());
     }
 
     @Test
@@ -62,12 +68,7 @@ public class UserManagementSmokeTest {
     @Test
     public void shouldGetUserInformationById() {
         var user = createUser(getUserDataWithRandomEmail());
-        RequestSpecification req = given()
-                .basePath(ENDPOINT_WITH_ID)
-                .baseUri(HOST)
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + TOKEN)
-                .pathParam("userId", user.id());
+        req.basePath(ENDPOINT_WITH_ID).pathParam("userId", user.id());
         UserResponseDto userFromGet = req.get().then().extract().as(UserResponseDto.class);
         Assert.assertEquals(user.name(), userFromGet.name());
     }
@@ -77,17 +78,11 @@ public class UserManagementSmokeTest {
         var user = createUser(getUserDataWithRandomEmail());
         String newEmail = "some_email%s@gmail.com".formatted(RandomStringUtils.randomAlphanumeric(5));
         logger.info("Generated new user email: {}", newEmail);
-        RequestSpecification req = given()
-                .basePath(ENDPOINT_WITH_ID)
-                .baseUri(HOST)
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + TOKEN)
-                .pathParam("userId", user.id());
         UserRequestDto updateUserPayload = new UserRequestDto(
                 user.name(), newEmail, user.gender(), user.status()
         );
-        UserResponseDto updateResponse = req.body(updateUserPayload)
-                .put().as(UserResponseDto.class);
+        req.basePath(ENDPOINT_WITH_ID).pathParam("userId", user.id()).body(updateUserPayload);
+        UserResponseDto updateResponse = req.put().as(UserResponseDto.class);
         logger.info("User after update: {}", updateResponse);
         Assert.assertEquals(newEmail, updateResponse.email());
     }
@@ -95,34 +90,28 @@ public class UserManagementSmokeTest {
     @Test
     public void shouldDeleteUserFromSystem() {
         var user = createUser(getUserDataWithRandomEmail());
-        RequestSpecification req = given()
-                .basePath(ENDPOINT_WITH_ID)
-                .baseUri(HOST)
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + TOKEN)
-                .pathParam("userId", user.id());
+        req.basePath(ENDPOINT_WITH_ID).pathParam("userId", user.id());
         req.delete().then().assertThat().statusCode(204);
         req.get().then().assertThat().statusCode(404);
     }
 
     @Test
     public void validateUserDataAgainstSchema() {
-        
+        var user = createUser(getUserDataWithRandomEmail());
+        logger.info("Validating user against schema '{}'", "user_schema.json");
+        req.basePath(ENDPOINT_WITH_ID).pathParam("userId", user.id());
+        req.get().then()
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("user_schema.json"));
     }
 
-    private static UserResponseDto createUser(UserRequestDto userData){
-        RequestSpecification req = given()
-                .basePath(ENDPOINT)
-                .baseUri(HOST)
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + TOKEN)
-                .body(userData);
+    private UserResponseDto createUser(UserRequestDto userData) {
+        req.body(userData);
         Response response = req.post().andReturn();
         logger.info("Created user: {}", response.prettyPrint());
         return response.then().extract().as(UserResponseDto.class);
     }
 
-    private static UserRequestDto getUserDataWithRandomEmail() {
+    private UserRequestDto getUserDataWithRandomEmail() {
         String randomEmailSuffix = RandomStringUtils.randomAlphanumeric(5);
         UserRequestDto payload = new UserRequestDto(
                 "Ivan Paulouski",
